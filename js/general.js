5b74d191f3505149122fe462a3ed6505820d18ec
Philippe Favre <phf.webcreation@gmail.com>
2024-09-27 08:36:16 -0700

    remove page

--- js/general.js
+++
@@ -1,8 +0,0 @@
-
-$('a[href^="#"]').click(function() {
-    var target = parseInt($(this).offset().top);
-    $('html, body').animate({
-      scrollTop: target - 150
-    }, "slow");
-    return false;
-});
